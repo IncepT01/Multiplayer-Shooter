b1625528281de21aeb25f5190ef8ed8adb305cad
IncepT01 <tiszagergo10@gmail.com>
2025-03-30 16:16:37 +0200

    Timer Synchronization

--- MultiplayerShooter/Source/MultiplayerShooter/PlayerController/MyPlayerController.cpp
+++ MultiplayerShooter/Source/MultiplayerShooter/PlayerController/MyPlayerController.cpp
@@ -20,6 +20,17 @@ void AMyPlayerController::Tick(float DeltaTime)
  	Super::Tick(DeltaTime);
  
  	SetHUDTime();
+ 	CheckTimeSync(DeltaTime);
+ }
+
+void AMyPlayerController::CheckTimeSync(float DeltaTime)
+ {
+ 	TimeSyncRunningTime += DeltaTime;
+ 	if (IsLocalController() && TimeSyncRunningTime > TimeSyncFrequency)
+ 	{
+ 		ServerRequestServerTime(GetWorld()->GetTimeSeconds());
+ 		TimeSyncRunningTime = 0.f;
+ 	}
  }
 
 void AMyPlayerController::OnPossess(APawn* InPawn)
@@ -118,11 +129,39 @@ void AMyPlayerController::SetHUDMatchCountdown(float CountdownTime)
 
 void AMyPlayerController::SetHUDTime()
  {
- 	uint32 SecondsLeft = FMath::CeilToInt(MatchTime - GetWorld()->GetTimeSeconds());
+ 	uint32 SecondsLeft = FMath::CeilToInt(MatchTime - GetServerTime());
  	if (CountdownInt != SecondsLeft)
  	{
- 		SetHUDMatchCountdown(MatchTime - GetWorld()->GetTimeSeconds());
+ 		SetHUDMatchCountdown(MatchTime - GetServerTime());
  	}
  
  	CountdownInt = SecondsLeft;
+ }
+
+void AMyPlayerController::ServerRequestServerTime_Implementation(float TimeOfClientRequest)
+ {
+ 	float ServerTimeOfReceipt = GetWorld()->GetTimeSeconds();
+ 	ClientReportServerTime(TimeOfClientRequest, ServerTimeOfReceipt);
+ }
+ 
+void AMyPlayerController::ClientReportServerTime_Implementation(float TimeOfClientRequest, float TimeServerReceivedClientRequest)
+ {
+ 	float RoundTripTime = GetWorld()->GetTimeSeconds() - TimeOfClientRequest;
+ 	float CurrentServerTime = TimeServerReceivedClientRequest + (0.5f * RoundTripTime);
+ 	ClientServerDelta = CurrentServerTime - GetWorld()->GetTimeSeconds();
+ }
+ 
+float AMyPlayerController::GetServerTime()
+ {
+ 	if (HasAuthority()) return GetWorld()->GetTimeSeconds();
+ 	else return GetWorld()->GetTimeSeconds() + ClientServerDelta;
+ }
+ 
+void AMyPlayerController::ReceivedPlayer()
+ {
+ 	Super::ReceivedPlayer();
+ 	if (IsLocalController())
+ 	{
+ 		ServerRequestServerTime(GetWorld()->GetTimeSeconds());
+ 	}
  }
